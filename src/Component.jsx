f224fa8865598676eec0483ca30c920f2390b670
Sriram Gorthi <sriramgorthi06@gmail.com>
2025-08-14 10:59:11 +0530

    default files

---
+++ src/Component.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+import './Component.css'
+
+const FunctionComponent = () =>{
+    return(
+        <div className='main'>
+        <div className='header'></div>
+        <div className='body'></div>
+        <div className='footer'></div>
+        </div>
+    )
+}
+export default FunctionComponent;
